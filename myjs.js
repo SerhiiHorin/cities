134cf36b7c73084c6b25e1b3c6d20aeb3e630e15
SerhiiHorin <mr.bladeee@gmail.com>
2016-12-11 11:51:48 +0000

    add new file

--- myjs.js
+++ myjs.js
@@ -3,7 +3,7 @@
 
    
 
-
+/* 
 
 $(document).ready(function(){
     // Activate Carousel
@@ -53,3 +53,30 @@ $(document).ready(function(){
         caption.css('display', 'none');
     });
 });
+  
+   */
+
+/*owlCarousel*/   
+ $(document).ready(function () {
+    $("#owl-demo").owlCarousel({
+        
+        itemsCustom : [
+        [0, 2],
+        [450, 2],
+        [600, 3],
+        [700, 3],
+        [1000, 4],
+        [1200, 4],
+        [1400, 5],
+        [1600, 7]
+      ],
+        navigation: true,
+        navigationText: [
+      "<span class='glyphicon glyphicon-menu-left'></span>",
+      "<span class='glyphicon glyphicon-menu-right'></span>"
+      ],
+        pagination: true,
+        slideSpeed : 700,
+        paginationSpeed : 700
+    });
+});	    
